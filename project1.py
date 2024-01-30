1f246cd79e24312f27b798d48f65dd6891604962
presstwice <38543504+presstwice@users.noreply.github.com>
2024-01-31 09:55:28 +1000

    x

--- project1.py
+++ project1.py
@@ -9,3 +9,18 @@ def roll():
 
 value = roll()
 print(value)
+
+while True:
+    players = input("Enter the number of players (2 - 4): ")
+    if players.isdigit():
+        players = int(players)
+        if 1 <= players <= 4:
+            break
+        else: 
+            print("Must be between 2 - 4 players.")
+
+    else: 
+        print("Invalid, try again.")
+        
+max_score = 50      
+print(players)
