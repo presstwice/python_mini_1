ff7fe68278a950cc2afd81b064b3ff081ac928ed
presstwice <38543504+presstwice@users.noreply.github.com>
2024-02-01 07:39:43 +1000

    x

--- project1.py
+++ project1.py
@@ -25,4 +25,20 @@ while True:
 max_score = 50   
 player_scores = [0 for _ in range(players)]
    
-print(player_scores)
+while max(player_scores) < max_score:
+    
+    current_score = 0
+    
+    should_roll = input("would you like to roll (y)?")
+    if should_roll.lower() != "y":
+        break
+    
+    value = roll()
+    if value == 0:
+        print("You rolled a 1! Turn done!")
+        break
+    else:
+        current_score == value
+        print("You rolled a:", value)
+
+    print("Your score is:", current_score)
