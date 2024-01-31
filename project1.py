6f4f98e69399d6deaf1118951c7effd6dc55bdf8
presstwice <38543504+presstwice@users.noreply.github.com>
2024-02-01 07:53:32 +1000

    x

--- project1.py
+++ project1.py
@@ -26,26 +26,25 @@ max_score = 50
 player_scores = [0 for _ in range(players)]
    
 while max(player_scores) < max_score:
-    
     for player_idx in range(players):
-        print("Player", player_idx + 1, "turn has just started!")
+        print("\n Player", player_idx + 1, "turn has just started! \n")
         current_score = 0
     
         while True:
             should_roll = input("would you like to roll (y)?")
             if should_roll.lower() != "y":
                 break
-            
+                        
             value = roll()
-            if value == 0:
+            if value == 1:
                 print("You rolled a 1! Turn done!")
                 current_score = 0
                 break
             else:
-                current_score == value
+                current_score += value
                 print("You rolled a:", value)
 
             print("Your score is:", current_score)
         
-        player_scores[player_idx] == current_score
+        player_scores[player_idx] += current_score
         print("Your total score is:", player_scores[player_idx])
